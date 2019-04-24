85fcc0d219aa0d904ddde59acf58b5e3c0fdabc0
Matthew Kennedy <burnsidemk@gmail.com>
2019-04-24 13:35:49 -0500

    Test a callback

--- hello.lisp
+++ hello.lisp
@@ -1,9 +1,14 @@
 (defpackage #:iup-hello
   (:use #:common-lisp))
+
 (in-package #:iup-hello)
+
 (defun main ()
   (iup:with-iup ()
-    (let* ((button (iup:button :title "Hello"))
+    (let* ((button (iup:button :title "Hello"
+			       :action (lambda (handle)
+					 (iup:message "Hello" (lisp-implementation-type))
+					 iup:+default+)))
 	   (dialog (iup:dialog button)))
       (iup:show dialog)
       (iup:main-loop))))
